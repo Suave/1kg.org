19d56993243b1cef0614d0fc55e87b3771f131a5
Suave Su <suave.su@gmail.com>
2008-10-29 14:56:22 +0800

    done topic post in city board

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,27 @@
+class TopicsController < ApplicationController
+  before_filter :login_required, :expect => [:show, :index]
+  
+  def new
+    @board = Board.find(params[:board_id])
+    if @board.talkable.class == CityBoard
+      @board_name = @board.talkable.geo_name + "同城"
+    end
+    @topic = Topic.new
+  end
+  
+  def create
+    @board = Board.find(params[:board_id])
+    @topic = Topic.new(params[:topic])
+    @topic.user = current_user
+    @topic.board = @board
+    @topic.save!
+    flash[:notice] = "发帖成功"
+    redirect_to board_url(@board)
+  end
+  
+  def show
+    @board = Board.find(params[:board_id])
+    @topic = Topic.find(params[:id])
+  end
+  
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,14 @@
 class Topic < ActiveRecord::Base
+  include BodyFormat
+  
   belongs_to :board
+  belongs_to :user
+  
+  before_save :format_content
+  
+  private
+  def format_content
+    body.strip! if body.respond_to?(:strip!)
+    self.body_html = body.blank? ? '' : formatting_body_html(body)
+  end
 end
