2410f10c5cb5f14811c2822da96f924e3d763cdd
Suave Su <suave.su@gmail.com>
2009-05-14 20:14:28 +0800

    remove rack

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
-  config.gem "rack"
+  #config.gem "rack"
   config.gem "fastercsv"
   #config.gem "ruby-aes"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
