294df40d29e294ede413f6f79e1b0f58464dccb3
Suave Su <suave.su@gmail.com>
2009-05-15 23:49:01 +0800

    change default avatar

---
+++ app/helpers/minisite/postcard/dashboard_helper.rb
@@ -0,0 +1,10 @@
+module Minisite::Postcard::DashboardHelper
+  def postcard_avatar_for(user, size)
+    if user.avatar.blank?
+      image_tag "/images/postcard/avatar_#{size}.png", :class => "avatar", :alt => user.login
+    else
+      image_tag url_for_file_column(user, :avatar, size), :class => "avatar", :alt => user.login
+    end
+  end
+  
+end
