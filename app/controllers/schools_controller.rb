534bc9aa015089864321484a9abd530d2cb1ce42
Suave Su <suave.su@gmail.com>
2009-06-30 11:31:11 +0800

    schoo info bug fix

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -10,12 +10,6 @@ class SchoolsController < ApplicationController
     respond_to do |format|
       @schools = School.recent_upload
       format.html {
-        @traffic = @school.traffic
-        @need = @school.need
-        @local   = @school.local
-        @contact = @school.contact
-        @finder  = @school.finder
-        @basic = @school.basic
         @topics = Topic.last_10_updated_topics(SchoolBoard)
         @photos = Photo.find(:all, :conditions => ["photos.school_id is not null"], :order => "updated_at desc", :limit => 12)
         
@@ -183,6 +177,13 @@ class SchoolsController < ApplicationController
   
   def show
     @school = School.find(params[:id])
+    
+    @traffic = @school.traffic
+    @need = @school.need
+    @local   = @school.local
+    @contact = @school.contact
+    @finder  = @school.finder
+    @basic = @school.basic
     
     if @school.nil? or @school.deleted?
       render_404 and return
