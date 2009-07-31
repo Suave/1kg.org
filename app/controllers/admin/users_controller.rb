412b0fb8623b738b33893928cf107ce336d2d5ed
Suave Su <suave.su@gmail.com>
2009-07-31 18:00:40 +0800

    school moderator manage bug fix, add private message notification

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -51,17 +51,7 @@ class Admin::UsersController < Admin::BaseController
       @user.roles << Role.find_by_identifier("roles.schools.moderator")
       flash[:notice] = "设置 #{@user.login} 为学校管理员"
       redirect_to admin_users_path(:type => "school")
-      
-    elsif params[:add] == "school"
-      school = School.find params[:school] unless params[:school].blank?
-      if @user && school
-        @user.roles << Role.find_by_identifier("roles.school.moderator.#{school.id}")
-        flash[:notice] = "已将 #{@user.login} 设置为 #{school.title} 的爱心大使"
-      else
-        flash[:notice] = "系统错误"
-      end
-      redirect_to moderator_school_url(school)
-      
+            
     elsif params[:add] == "city"
       geo = Geo.find(params[:geo]) unless params[:geo].blank?
       
@@ -98,12 +88,6 @@ class Admin::UsersController < Admin::BaseController
       @user.roles.delete(Role.find_by_identifier("roles.schools.moderator"))
       flash[:notice] = "取消了 #{@user.login} 学校管理员权限"
       redirect_to admin_users_path(:type => "school")
-      
-    elsif params[:remove] == "school"
-      school = School.find params[:school] unless params[:school].blank?
-      @user.roles.delete(Role.find_by_identifier("roles.school.moderator.#{school.id}"))
-      flash[:notice] = "已经取消 #{@user.login} 的爱心大使身份"
-      redirect_to moderator_school_url school
       
     elsif params[:remove] == "admin"
       admin_count = User.admins.size
