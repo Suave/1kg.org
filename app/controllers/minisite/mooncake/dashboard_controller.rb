eb2208c1f316d630e42176e8c7c8d1241b07f23f
Suave Su <suave.su@gmail.com>
2009-08-26 12:34:50 +0800

    trival update

--- app/controllers/minisite/mooncake/dashboard_controller.rb
+++ app/controllers/minisite/mooncake/dashboard_controller.rb
@@ -1,19 +1,25 @@
 require 'uuid'
 
 class Minisite::Mooncake::DashboardController < ApplicationController
-  before_filter :login_required, :except => [:index, :reserve, :buy]
+  before_filter :login_required, :except => [:index, :reserve, :buy, :password]
   
   def index
-    @reserve = Reserve.new
+    #@reserve = Reserve.new
     render :action => "new"
   end
   
   def reserve
-    @reserve = Reserve.new params[:reserve]
-    @reserve.save!
+    #@reserve = Reserve.new params[:reserve]
+    #@reserve.save!
     #flash[:notice] = "已经收到您的预定，面市后我们会及时通知您"
     #redirect_to minisite_mooncake_index_url
   end
+  
+  def password
+    flash[:notice] = "密码配对功能9月10日启用"
+    redirect_to index_path
+  end
+  
   
   def buy
     
