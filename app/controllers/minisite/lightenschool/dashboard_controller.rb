4840b98b73fa5465f8e773a26d79cc8ccc8c7a5c
makestory <makestory@zoho.com>
2009-09-11 01:23:14 +0800

    add lightenschool group

--- app/controllers/minisite/lightenschool/dashboard_controller.rb
+++ app/controllers/minisite/lightenschool/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Minisite::Lightenschool::DashboardController < ApplicationController
   
   def index
+    @group = Group.find_by_slug('guide')
+    @board = @group.discussion.board
     
   end
   
