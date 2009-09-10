8657fb169f9291caff62d1526f5c309e5fe7bef5
makestory <makestory@zoho.com>
2009-09-10 14:05:16 +0800

    mooncake avatar & hack CSS for IE

--- app/helpers/minisite/mooncake/dashboard_helper.rb
+++ app/helpers/minisite/mooncake/dashboard_helper.rb
@@ -1,7 +1,7 @@
 module Minisite::Mooncake::DashboardHelper
   def mooncake_avatar_for(user, size)
     if user.avatar.blank?
-      image_tag "/images/postcard/avatar_#{size}.png", :class => "avatar", :alt => user.login
+      image_tag "/images/mooncake/avatar_#{size}.png", :class => "avatar", :alt => user.login
     else
       image_tag url_for_file_column(user, :avatar, size), :class => "avatar", :alt => user.login
     end
