011b1e42627e78d05f51eff63bdc9c1a288e423d
Zhang Yuanyi <yzhang@elctech.com>
2009-09-18 14:33:43 +0800

    增加标签搜索及所有标签页面

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,15 @@
 class TagsController < ApplicationController
+  def index
+    respond_to do |format|
+      if params[:tag]
+        format.html {redirect_to tag_path(:tag => params[:tag])}
+      else
+        @tags = SchoolNeed.tag_counts + SchoolGuide.tag_counts
+        format.html
+      end
+    end
+  end
+  
   def show
     @tag = params[:tag]
     @school_needs = SchoolNeed.find_tagged_with(params[:tag])
