3ca2518222e580fdb0058e1161d08345b4c00957
Zhang Yuanyi <yzhang@elctech.com>
2009-09-23 13:58:59 +0800

    点亮学校首页只显示最新的5个攻略

--- app/models/school_guide.rb
+++ app/models/school_guide.rb
@@ -14,7 +14,9 @@ class SchoolGuide < ActiveRecord::Base
   attr_accessible :title, :content, :tag_list, :school_id, :user_id, :last_replied_at, :last_replied_by_id, :comments_count
   
   after_create :initial_last_replied
-    
+  
+  named_scope :recent, :limit => 5, :order => 'created_at DESC'
+  
   private
   def initial_last_replied
     self.update_attributes!(:last_replied_at => self.created_at, :last_replied_by_id => self.user_id)
