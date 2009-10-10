5d04da4fec990456c9b752783905e0fd086c517c
makestory <makestory@zoho.com>
2009-10-11 05:44:01 +0800

    修改学校提交界面＆学校信息

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -1,6 +1,6 @@
 module SchoolsHelper
   def radio_choice(form, object)
-    [[0,"否"], [1,"是"], [2, "未知"]].collect {|r| form.radio_button(object, r[0]) + r[1]}
+    [[0,"无"], [1,"有"], [2, "未知"]].collect {|r| form.radio_button(object, r[0]) + r[1]}
   end
   
   def radio_value(value)
