43072ba29049b8ea69b4123afab7b91ee95528d6
Suave Su <suave.su@gmail.com>
2009-11-04 11:56:31 +0800

    网站公告可以直接转向 可以评论

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -5,6 +5,13 @@ class BulletinsController < ApplicationController
   
   def show
     @bulletin = Bulletin.find params[:id]
+    unless @bulletin.redirect_url.blank?
+      redirect_to @bulletin.redirect_url
+    else
+      @comments = @bulletin.comments.available.paginate :page => params[:page] || 1, :per_page => 15
+      @comment = BulletinComment.new
+    end
+    
   end
   
   
