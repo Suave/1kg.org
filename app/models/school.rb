8c54dc8cc6b8eb181c8b1b33f6cf85de3cda009c
Zhang Yuanyi <yzhang@elctech.com>
2009-11-20 16:09:02 +0800

    修改分享和攻略的排序方法

--- app/models/school.rb
+++ app/models/school.rb
@@ -39,7 +39,7 @@ class School < ActiveRecord::Base
   
   has_one  :discussion, :class_name => "SchoolBoard", :dependent => :destroy
   has_many :shares, :order => "id desc", :dependent => :destroy
-  has_many :guides,  :class_name => "SchoolGuide", :dependent => :destroy, :order => 'updated_at DESC'
+  has_many :guides,  :class_name => "SchoolGuide", :dependent => :destroy
   has_many :photos, :order => "id desc", :dependent => :destroy
   belongs_to :main_photo, :class_name => 'Photo'
   has_many :visited, :dependent => :destroy
