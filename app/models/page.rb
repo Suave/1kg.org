0caf655089bde9c58337517e948d0278c6335a5d
Zhang Yuanyi <yzhang@elctech.com>
2009-11-23 14:37:45 +0800

    去除静态页面缓存

--- app/models/page.rb
+++ app/models/page.rb
@@ -19,12 +19,10 @@ class Page < ActiveRecord::Base
   validates_uniqueness_of :slug, :message => "slug 必须是唯一的"
   
   belongs_to :last_modified_by, :class_name => "User", :foreign_key => "last_modified_by_id"
-    
+  
   def to_param
     slug
   end
   
   private
-
-  
 end
