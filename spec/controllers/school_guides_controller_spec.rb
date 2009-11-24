af15d2ff1e5231fbd62dd4e65420dfdba4a9b7c5
Zhang Yuanyi <yzhang@elctech.com>
2009-11-24 16:47:13 +0800

    修复测试

---
+++ spec/controllers/school_guides_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe SchoolGuidesController do
+
+  #Delete this example and add some real ones
+  it "should use SchoolGuidesController" do
+    controller.should be_an_instance_of(SchoolGuidesController)
+  end
+
+end
