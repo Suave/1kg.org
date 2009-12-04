23649a5c9ea6e612c23ed3983298d9d98e6c1383
Zhang Yuanyi <yzhang@elctech.com>
2009-12-04 18:55:03 +0800

    增加SchoolGuide的测试

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,28 +8,6 @@ describe Comment do
     @user = User.first
   end
   
-  it "should not be editable if comment don't have an owner" do
-    @comment.user_id = nil
-    @comment.should_not be_editable_by(@user)
-  end
-  
-  it "should be editable if user is admin" do
-    @user.should_receive(:admin?).and_return(true)
-    @comment.user_id = 1234
-    @comment.should be_editable_by(@user)
-  end
-  
-  it "should be editable if comment is created by user" do
-    @comment.user_id = @user.id
-    @comment.should be_editable_by(@user)
-  end
-  
-  it "should not be editable if comment is not created by user and user isn't admin" do
-    @user.should_receive(:admin?).and_return(false)
-    @comment.user_id = 1234
-    @comment.should_not be_editable_by(@user)
-  end
-  
   describe 'Archives' do
     it "should only archive comments not deleted" do
       result = Comment.archives('Share')
