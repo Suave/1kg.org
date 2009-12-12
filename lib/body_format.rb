15b88a3725d5c56183a0f1d556ef0e56c20fea6b
Zhang Yuanyi <yzhang@elctech.com>
2009-12-12 18:01:58 +0800

    修复标签过滤和用户显示Bug

--- lib/body_format.rb
+++ lib/body_format.rb
@@ -1,8 +1,12 @@
 module BodyFormat
   def sanitize(html)
-    Sanitize.clean(html, :elements => ['a', 'div', 'span', 'img', 'p', 'embed',
-      'em', 'ol', 'ul', 'li', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'tt'],
+    begin
+      Sanitize.clean(html, :elements => ['a', 'div', 'span', 'img', 'p', 'embed',
+        'em', 'ol', 'ul', 'li', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'tt'],
           :attributes => {'a' => ['href', 'title'], 'img' => ['src', 'alt', 'title'], 'span' => ['style']},
           :protocols => {'a' => {'href' => ['http', 'https', 'mailto']}})
+    rescue
+      html
+    end
   end
 end
