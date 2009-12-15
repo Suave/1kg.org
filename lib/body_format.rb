5e95da7ec1b62eaa731fa4f16a4720f1f6b2c630
Zhang Yuanyi <yzhang@elctech.com>
2009-12-15 15:55:10 +0800

    修复标签过滤的Bug

--- lib/body_format.rb
+++ lib/body_format.rb
@@ -1,10 +1,9 @@
 module BodyFormat
   def sanitize(html)
     begin
-      Sanitize.clean(html, :elements => ['a', 'div', 'span', 'img', 'p', 'embed',
+      Sanitize.clean(html, :elements => ['a', 'div', 'span', 'img', 'p', 'embed', 'br',
         'em', 'ol', 'ul', 'li', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'tt'],
-          :attributes => {'a' => ['href', 'title'], 'img' => ['src', 'alt', 'title'], 'span' => ['style']},
-          :protocols => {'a' => {'href' => ['http', 'https', 'mailto']}})
+          :attributes => {'a' => ['href', 'target', 'title'], 'img' => ['src', 'alt', 'title'], 'span' => ['style']})
     rescue
       html
     end
