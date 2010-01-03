2b001c954d5227defe64f3ec3886e05680ea0aa6
Zhang Yuanyi <yzhang@elctech.com>
2010-01-03 19:09:41 +0800

    修复站内信换行和修改邮件发送者名称

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,7 +1,7 @@
 class Mailer < ActionMailer::Base
   def message_notification(message_copy)
     @recipients = message_copy.recipient.email
-    @from       = "no-reply@1kg.org"
+    @from       = "1KG<no-reply@1kg.org>"
     @subject    = "[多背一公斤]收到新的站内信"
     @sent_on    = Time.now
     @body       = {:message => message_copy.message,
@@ -11,7 +11,7 @@ class Mailer < ActionMailer::Base
   
   def new_password_notification(user, new_password)
     @recipients = user.email
-    @from       = "no-reply@1kg.org"
+    @from       = "1KG<no-reply@1kg.org>"
     @subject    = "[多背一公斤]您的新帐户密码"
     @sent_on    = Time.now
     @body       = {:new_password => new_password}
@@ -19,7 +19,7 @@ class Mailer < ActionMailer::Base
   
   def submitted_school_notification(school)
     @recipients  = ENV["RAILS_ENV"] == "production" ? "newschools@googlegroups.com" : "suave.su@gmail.com,zhangyuanyi@gmail.com"
-    @from        = "no-reply@1kg.org"
+    @from        = "1KG<no-reply@1kg.org>"
     @subject     = "用户提交了一所新学校，请审核"
     @sent_on     = Time.now
     @body        = {:school => school}
@@ -27,7 +27,7 @@ class Mailer < ActionMailer::Base
   
   def destroyed_school_notification(school)
     @recipients  = ENV["RAILS_ENV"] == "production" ? school.user.email : "suave.su@gmail.com,zhangyuanyi@gmail.com"
-    @from        = "no-reply@1kg.org"
+    @from        = "1KG<no-reply@1kg.org>"
     @subject     = "对不起，您提交的#{school.title}已被管理员删除"
     @sent_on     = Time.now
     @body        = {:school => school}
@@ -35,7 +35,7 @@ class Mailer < ActionMailer::Base
   
   def invalid_school_notification(school)
     @recipients  = ENV["RAILS_ENV"] == "production" ? school.user.email : "suave.su@gmail.com,zhangyuanyi@gmail.com"
-    @from        = "no-reply@1kg.org"
+    @from        = "1KG<no-reply@1kg.org>"
     @subject     = "对不起，您提交的#{school.title}没有通过审核，请您对学校信息进行补充"
     @sent_on     = Time.now
     @body        = {:school => school}
@@ -43,11 +43,9 @@ class Mailer < ActionMailer::Base
   
   def create_default_user_for_mooncake(user, password)
     @recipients = ENV["RAILS_ENV"] == "production" ? user.email : "suave.su@gmail.com"
-    @from       = "no-reply@1kg.org"
+    @from       = "1KG<no-reply@1kg.org>"
     @subject    = "欢迎加入1KG.org，请查看您的用户信息"
     @sent_on    = Time.now
     @body       = {:user => user, :password => password}
-    
   end
-  
 end
