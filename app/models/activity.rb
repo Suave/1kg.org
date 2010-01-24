309a01e080a2a114a190ff1af2468117930623bf
makestory <makestory@zoho.com>
2010-01-24 23:18:34 +0800

    活动开始日期可以是当天

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -82,7 +82,7 @@ class Activity < ActiveRecord::Base
   validates_presence_of :description_html, :message => "活动介绍是必填项"
   
   def validate
-    unless  (Time.now <= start_at)&&(start_at <= end_at)&&(end_at <= start_at + 3.month )
+    unless  ((Time.now - 1.day)<= start_at)&&(start_at <= end_at)&&(end_at <= start_at + 3.month )
       errors.add(:time,"日期填写不正确　")
     end
   end
