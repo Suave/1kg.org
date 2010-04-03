c8e8e9af6e482f6c4189f67fa10150b7377f8516
makestory <makestory@zoho.com>
2010-04-04 00:23:00 +0800

    [#354 state:resolved]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -251,9 +251,9 @@ module ApplicationHelper
   def share_link(text)
     
       html = "<div class='share_link'> 分享这个页面到 "
-      html += " <a target='_blank' href='http://www.douban.com/recommend/?url=#{request.url}&title=#{@title}' ><img src='/images/icon/douban.gif' alt='分享到豆瓣'/> 豆瓣</a>"
-      html += " <a target='_blank' href='http://share.renren.com/share/buttonshare.do?link=#{request.url}&title=#{@title}' ><img src='/images/icon/renren.gif' alt='分享到人人'/> 人人</a>"
-      html += " <a target='_blank' href='http://www.kaixin001.com/repaste/share.php?rurl=#{request.url}&rtitle=#{@title}&rcontent=#{text}'  ><img src='/images/icon/kaixin001.gif' alt='分享到开心'/> 开心</a>"
+      html += " <a target='_blank' href='http://www.douban.com/recommend/?url=#{request.url}&title=#{@title}-多背一公斤' ><img src='/images/icon/douban.gif' alt='分享到豆瓣'/> 豆瓣</a>"
+      html += " <a target='_blank' href='http://share.renren.com/share/buttonshare.do?link=#{request.url}&title=#{@title}-多背一公斤' ><img src='/images/icon/renren.gif' alt='分享到人人'/> 人人</a>"
+      html += " <a target='_blank' href='http://www.kaixin001.com/repaste/share.php?rurl=#{request.url}&rtitle=#{@title}-多背一公斤&rcontent=#{text}...'  ><img src='/images/icon/kaixin001.gif' alt='分享到开心'/> 开心</a>"
       html += "</div>"
   end
   def short_title(something,long=22)
