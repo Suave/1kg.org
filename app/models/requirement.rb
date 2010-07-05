0510f0168a2f190f3110bc43f4cd63ecd05c8038
makestory <makestory@zoho.com>
2010-07-05 16:17:04 +0800

    为团捐增加最后修改

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -56,7 +56,7 @@ class Requirement < ActiveRecord::Base
   end
   
   def last_updated_at
-    [self.created_at,self.last_modified_at,(self.shares.empty? ? nil : self.shares.last.created_at)].compact.max
+    [self.created_at,self.last_modified_at,(self.shares.empty? ? nil : self.shares.last.created_at),(self.photos.empty? ? nil : self.photos.last.created_at)].compact.max
   end
   
   def matched_percent_str
