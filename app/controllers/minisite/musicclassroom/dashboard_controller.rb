ffe66b90c2554f73339385f974d125ba7bba38a9
makestory <makestory@zoho.com>
2010-07-13 18:00:05 +0800

    修改文字，去掉小组，增加分享汇总。

--- app/controllers/minisite/musicclassroom/dashboard_controller.rb
+++ app/controllers/minisite/musicclassroom/dashboard_controller.rb
@@ -1,10 +1,9 @@
 class Minisite::Musicclassroom::DashboardController < ApplicationController
   
   def index
-    @group = Group.find_by_slug('postcard')
-    @board = @group.discussion.board
     @project = RequirementType.last
     @photos = Photo.with_school.find(:all,:limit => 8,:order => "created_at desc", :group => "school_id")
+    @shares = Share.find(:all,:limit => 5)
   end
   
 end
