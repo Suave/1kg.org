a04dfd9126edd5a9aa869250b64a093319562ad7
makestory <makestory@zoho.com>
2010-07-24 11:18:43 +0800

    团队页面修改第二期

--- app/controllers/neighbors_controller.rb
+++ app/controllers/neighbors_controller.rb
@@ -22,12 +22,9 @@ class NeighborsController < ApplicationController
   def destroy
     neighbor = User.find(params[:user_id])
     myself = User.find(params[:id])
-    
     @neighborhood = Neighborhood.delete_all(:user_id => myself.id, :neighbor_id => neighbor.id)
-    
     flash[:notice] = "您已取消了对#{neighbor.login}的关注"
     redirect_to user_url(neighbor)
-  end
-  
+  end  
   
 end
