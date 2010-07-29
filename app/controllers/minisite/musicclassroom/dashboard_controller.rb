29ffe7247625076c7a6d45a106c2d426c475111b
makestory <makestory@zoho.com>
2010-07-29 14:01:14 +0800

    关联“音乐教室”的公益项目

--- app/controllers/minisite/musicclassroom/dashboard_controller.rb
+++ app/controllers/minisite/musicclassroom/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class Minisite::Musicclassroom::DashboardController < ApplicationController
   
   def index
-    @project = RequirementType.last
-    @photos = Photo.with_school.find(:all,:limit => 8,:order => "created_at desc", :group => "school_id")
-    @shares = Share.find(:all,:limit => 5)
+    @project = RequirementType.find(10)
+    @photos = Photo.find(:all,:include => [:requirement],:conditions => ["stuff_bucks.type_id = ?",@project.id])
+    @shares = Share.find(:all,:include => [:requirement],:conditions => ["stuff_bucks.type_id = ?",@project.id])
   end
   
 end
