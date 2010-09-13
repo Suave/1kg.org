1602ebb073f9cb81bdb61a80fc3e761bee60e4c7
makestory <makestory@zoho.com>
2010-09-13 19:23:33 +0800

    Revert "remove preinitializer.rb"
    This reverts commit bf391826221451e352b29c60639171f3ea75be27.

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,4 +85,17 @@ namespace :deploy do
       break if stream == :err    
     end
   end
+  
+  desc "installs Bundler if it is not already installed"
+  task :install_bundler, :roles => :app do
+    sudo "sh -c 'if [ -z `which bundle` ]; then echo Installing Bundler; sudo gem install bundler; fi'"
+  end
+
+  desc "run 'bundle install' to install Bundler's packaged gems for the current deploy"
+  task :bundle_install, :roles => :app do
+    run "cd #{release_path} && bundle install --without 'development test'"
+  end
 end
+
+before "deploy:bundle_install", "deploy:install_bundler"
+after "deploy:update_code", "deploy:bundle_install"
