1134420fb74ca8bad5fd4b69f26dad11270c0b87
loddit <loddit@gmail.com>
2011-09-05 16:29:57 +0800

    mangexingdong update

--- app/controllers/minisite/mangexingdong_controller.rb
+++ app/controllers/minisite/mangexingdong_controller.rb
@@ -1,6 +1,6 @@
 class Minisite::MangexingdongController < ApplicationController
   def index
-    @group = Group.find(:first,:conditions=>{:slug => 'mangexingdong'})
+    @group = Group.find(:first,:conditions=>{:slug => 'weixingfu'})
     @board = @group.discussion.board
   end
 end
