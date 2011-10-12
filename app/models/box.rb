e285f3c32de47951189f1386075b8cbf9e9107b7
loddit <loddit@gmail.com>
2011-10-12 16:54:22 +0800

    fix

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,6 +1,6 @@
 class Box < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => {:box_topic => "300x300#", :box_avatar => "150x150#" }
+  has_attached_file :image, :styles => {:box_avatar => "72x72#", :box_topic => "140x140#" }
   has_attached_file :guide
   
   named_scope :available,     :conditions => {:available => true}
