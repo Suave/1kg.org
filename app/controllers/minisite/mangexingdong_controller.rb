013ebf37ffb8c1b74c742d35c43f4b2f852c3f23
loddit <loddit@gmail.com>
2011-10-14 10:50:10 +0800

    update counter

--- app/controllers/minisite/mangexingdong_controller.rb
+++ app/controllers/minisite/mangexingdong_controller.rb
@@ -1,21 +1,26 @@
 class Minisite::MangexingdongController < ApplicationController
-  before_filter :views_counter, :except => []
   
   def index
+    update_count('index_count')
     @group = Group.find(:first,:conditions=>{:slug => 'mangexingdong'})
     @board = @group.discussion.board
   end
   
   def poster 
+    update_count('poster_count')
   end
 
-  def views_counter
+  private
+
+  def update_count(count)
     File.open("#{RAILS_ROOT}/public/json/mangexingdong.json", 'r') do |file|
-      @views_count = JSON.parse(file.readlines.to_s)['views_count'] + 1
+      @json = JSON.parse(file.readlines.to_s)
     end
     File.open("#{RAILS_ROOT}/public/json/mangexingdong.json", 'w') do |file|
-      file.write({'views_count' => @views_count}.to_json)
+      file.write((@json.merge({count => @json[count] + 1})).to_json)
     end
-    @total_count = ((Time.now - 1.month.ago)/3600).to_i*15 + @views_count
+    @total_count = { 'index_count' => ((Time.now - 1.month.ago)/3600).to_i*15,
+      'poster_count' => ((Time.now - 1.month.ago)/3600).to_i*5
+    }[count] + 1 + @json[count]
   end
 end
