496e34914d1dabcf999371277f3d2679fc69af46
loddit <loddit@gmail.com>
2011-11-02 12:19:08 +0800

    fix

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -70,6 +70,7 @@ class BoxesController < ApplicationController
   def show
     @box = Box.find(params[:id])
     @boxes = Box.available - [@box]
+    @comment = Comment.new
     @comments = @box.comments.find(:all,:include => [:user,:commentable]).paginate :page => params[:page] || 1, :per_page => 20
   end
 end
