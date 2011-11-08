41a28e6e21d1bb4558d374ebe5dfaee85a99098e
loddit <loddit@gmail.com>
2011-11-08 14:12:27 +0800

    bug

--- app/models/school_observer.rb
+++ app/models/school_observer.rb
@@ -1,6 +1,6 @@
 class SchoolObserver < ActiveRecord::Observer
   def after_create(school)
-    School.manangements.build(:user_id => schools.user_id).save
+    school.manangements.build(:user_id => schools.user_id).save
     # 邮件通知管理员有新学校提交
     Mailer.deliver_submitted_school_notification(school) if school.user
   end
