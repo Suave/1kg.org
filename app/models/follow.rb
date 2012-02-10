647d341c418d79a09c9fe7264a866f10836e844c
loddit <loddit@gmail.com>
2012-02-11 00:52:19 +0800

    add validation for follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,11 @@ class Follow < ActiveRecord::Base
   belongs_to :followable, :polymorphic => true
   belongs_to :user
   
+  validates_presence_of :user_id       
+  validates_uniqueness_of :user_id,   :scope => [:followable_type,:followable_id]
+  validates_presence_of :followable_type
+  validates_presence_of :followable_id,
+  
   named_scope :to_school, :conditions => {:followable_type => 'School'}
   named_scope :to_user, :conditions => {:followable_type => 'User'},:include => :followable
   named_scope :to_team, :conditions => {:followable_type => 'Team'}
