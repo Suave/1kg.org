4d03e7f7346ca341b2f9005e971e3e45165254f9
loddit <loddit@gmail.com>
2012-05-13 01:25:42 +0800

    spam

--- lib/anti_spam.rb
+++ lib/anti_spam.rb
@@ -1,8 +1,8 @@
 module AntiSpam
-  SPAMWORD = %w(　 小姐 考级 考试答案 考前答案 性服务 小妹 淘宝 媛交 按摩服务 全套服务 特殊服务 妓 夜情 你妹 找美女)
+  SPAMWORD = %w(　 小姐 考级 考试答案 考前答案 性服务 小妹 淘宝 媛交 按摩服务 全套服务 特殊服务 妓 夜情 你妹 找美女 学生妹 淘宝商城 三陪)
   def check_spam_word_for(entry,attr)
     SPAMWORD.each do |w|
-      return true if entry.attributes[attr].include? w
+      return true if entry.attributes[attr].gsub(/ /,'').include? w
     end
     false
   end
