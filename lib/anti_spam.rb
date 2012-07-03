4a5015398c3eddc309d4610a6b8490d7670a95c1
loddit <loddit@gmail.com>
2012-07-03 11:15:29 +0800

    anti spam

--- lib/anti_spam.rb
+++ lib/anti_spam.rb
@@ -2,7 +2,7 @@ module AntiSpam
   SPAM_WORDS = APP_CONFIG["spam_words"].split
   def check_spam_word_for(entry,attr)
     SPAM_WORDS.each do |w|
-      return true if entry.attributes[attr].gsub(/ /,'').include? w
+      return true if entry.attributes[attr].gsub(/ |　/,'').include? w
     end
     false
   end
