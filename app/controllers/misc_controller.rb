6106ed77b81d554af43d371457ada44e4490a848
Suave Su <suave.su@gmail.com>
2013-12-10 15:53:59 +0800

    remove lots from user homepage, it's empty TOT

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -15,15 +15,15 @@ class MiscController < ApplicationController
   
   def public_look
     @page_title = "首页"
-    @activity_count = Activity.ongoing.size
-    @hot_activities = Activity.ongoing.find(:all,:limit => 4,:order => "participations_count desc" ,:conditions => ["main_photo_id is not null and created_at > ?",1.months.ago])
-    @voteds = Vote.find(:all,:order => "created_at desc",:limit => 40).map(&:voteable).uniq[0..10]
-    @co_donations = CoDonation.validated.ongoing.all(:limit => 2)
-    @teams = Team.validated.find(:all,:order => "created_at desc",:limit => 6)
+#    @activity_count = Activity.ongoing.size
+#    @hot_activities = Activity.ongoing.find(:all,:limit => 4,:order => "participations_count desc" ,:conditions => ["main_photo_id is not null and created_at > ?",1.months.ago])
+#    @voteds = Vote.find(:all,:order => "created_at desc",:limit => 40).map(&:voteable).uniq[0..10]
+#    @co_donations = CoDonation.validated.ongoing.all(:limit => 2)
+#    @teams = Team.validated.find(:all,:order => "created_at desc",:limit => 6)
     @bulletins = Bulletin.recent
     @visits = Visited.latestvisit
     @wannas = Visited.latestwanna
-    @projects = Project.find(:all,:limit => 2,:order => "created_at desc",:conditions => ['id != ?',10]) #hack for operation
+#    @projects = Project.find(:all,:limit => 2,:order => "created_at desc",:conditions => ['id != ?',10]) #hack for operation
     
     respond_to do |wants|
       wants.html{render :action => "index"}
